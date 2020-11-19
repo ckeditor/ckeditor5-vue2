0c51aca26ffe663a83595baaba24a15e2c22a3d3
Paweł Smyrek <p.smyrek@cksource.com>
2020-11-19 13:47:07 +0100

    Added Vue version check

--- src/plugin.js
+++ src/plugin.js
@@ -3,6 +3,9 @@
  * For licensing, see LICENSE.md.
  */
 
+ /* globals console */
+
+import Vue from 'vue';
 import CKEditorComponent from './ckeditor.js';
 
 const CKEditor = {
@@ -17,4 +20,28 @@ const CKEditor = {
 	component: CKEditorComponent
 };
 
-export default CKEditor;
+/**
+ * Checks if CKEditor plugin supports currently installed Vue. This plugin supports only Vue 2.x.
+ * The CKEditor plugin for Vue 3.x can be found on https://github.com/ckeditor/ckeditor5-vue2.
+ *
+ * @param {string} version Current version of the Vue framework.
+ */
+function isVueSupported( version ) {
+	const currentVersion = parseInt( version );
+	const isSupported = currentVersion >= 2;
+	const isSupportedByThisRepo = currentVersion === 2;
+
+	if ( !isSupportedByThisRepo ) {
+		console.warn( 'This CKEditor plugin supports only Vue 2.x.' );
+
+		if ( isSupported ) {
+			console.warn(
+				`For Vue ${ version }, which you have currently installed, please visit https://github.com/ckeditor/ckeditor5-vue.`
+			);
+		}
+	}
+
+	return isSupportedByThisRepo;
+}
+
+export default isVueSupported( Vue.version ) ? CKEditor : {};
