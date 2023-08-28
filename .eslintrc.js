73ef4bc584b550b11092c53ae7ed1c47d2669536
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-08-28 17:15:04 +0200

    eslint: ignore the coverage/ directory.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@
 
 // Note: The ESLint configuration is mandatory for vue-cli.
 module.exports = {
+	'ignorePatterns': [
+		'coverage/**'
+	],
 	'extends': 'ckeditor5',
 	'rules': {
 		'ckeditor5-rules/license-header': [ 'error', { headerLines: [
